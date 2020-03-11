bf9d7a390623d22453b40201503cb670ff93b02b
felixbaumgaertner <accounts@felixbaumgaertner.de>
2020-03-11 11:35:57 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,26 @@
+=== Plugin Name ===
+Contributors: felixbaumgaertner
+Donate link: felixbaumgaertner.de
+Tags: cache, opcache, cache reset, opcache reset
+Requires at least: 5.0.0
+Tested up to: 5.4
+Stable tag: 5.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin that just adds an admin button for clearing the PHP OPcache if the server enabled it. That's it.
+
+== Description ==
+
+A plugin that just adds an admin button for clearing the PHP OPcache if the server enabled it. That's it.
+
+== Installation ==
+
+1. Upload the OPcache Clear plugin to to your website
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Find a button in the top admin bar (only if your server has OPcache enabled)
+
+== Changelog ==
+
+= 1.0 =
+* Basic functionality added, nothing special.
---
+++ opcache-clear.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @link              felixbaumgaertner.de
+ * @since             1.0.0
+ * @package           opcache-clear
+ *
+ * @wordpress-plugin
+ * Plugin Name:       OPcache Clear
+ * Description:       A plugin that just adds an admin button for clearing the PHP OPcache if the server enabled it. That's it.
+ * Version:           1.0.0
+ * Author:            Felix Baumgaertner
+ * Author URI:        felixbaumgaertner.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       opcache-clear
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+function run_opcc() {
+
+	// detect if cache clearing is requested
+	if ( isset($_GET['opcc']) ) {
+
+		add_action( 'admin_notices', 'opcc_notice' );
+		function opcc_notice() {
+			// run opcache_reset and display success or error notice
+			if ( opcache_reset() ) {
+				?>
+				<div class="notice notice-success">
+					<p><?php _e( 'OPcache cleared successfully.', 'opcache-clear' ); ?></p>
+				</div>
+				<?php
+			} else {
+				?>
+				<div class="notice notice-error">
+					<p><?php _e( 'Error: OPcache could not be cleared.', 'opcache-clear' ); ?></p>
+				</div>
+				<?php
+			}
+		}
+	}
+
+	// adds button to admin bar for easy access cache clearing
+	add_action( 'admin_bar_menu', 'add_toolbar_items', 96 );
+	function add_toolbar_items( $admin_bar ) {
+		if ( ini_get('opcache.enable') && opcache_get_status() ) {
+			$admin_bar->add_menu(
+				array(
+					'id'    => 'opcc',
+					'title' => 'Clear Cache',
+					'href'  => '#',
+					'meta'  => array(
+						'title'		=> __( 'Clears the current OPcache.', 'opcache-clear' ),	// tooltip
+						'onclick'	=> 'window.location.search += "&opcc=true";',	// functionality to reload site with $_GET parameter
+					),
+				)
+			);
+		}
+	}
+	
+}
+run_opcc();
